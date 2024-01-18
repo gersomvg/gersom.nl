c8baae0b5495a8eeda621790841929cb451a502e
Gersom van Ginkel <inbox@gersom.nl>
2024-01-18 12:14:37 +0100

    update monospace font for CV

--- src/routes/admin/cv/+page@.svelte
+++ src/routes/admin/cv/+page@.svelte
@@ -12,6 +12,10 @@
 		href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
 		rel="stylesheet"
 	/>
+	<link
+		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 <div class="ProseMirror">
 	<div class="flex justify-between">
@@ -38,5 +42,10 @@
 	.ProseMirror {
 		font-family: 'Inter', sans-serif;
 		font-optical-sizing: auto;
+	}
+
+	.ProseMirror :global(pre),
+	.ProseMirror :global(code) {
+		font-family: 'IBM Plex Mono', monospace;
 	}
 </style>
